b1bb2fd4da5084a2aa460cfcd7ce312a9f2b2237
kerekesroland <kerekes98roland@gmail.com>
2022-12-28 21:02:04 +0100

    feat(WIP): added the Product and Products pages and scss files

---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,9 @@
+import "./Product.scss";
+
+type Props = {};
+
+const Product = (props: Props) => {
+  return <div>Product</div>;
+};
+
+export default Product;
---
+++ src/pages/Products/Products.tsx
@@ -0,0 +1,9 @@
+import "./Products.scss";
+
+type Props = {};
+
+const Products = (props: Props) => {
+  return <div>Products</div>;
+};
+
+export default Products;
