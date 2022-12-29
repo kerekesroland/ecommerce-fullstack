8ab74e2bdcade486df82af72d02ebf5f4ed449b1
kerekesroland <kerekes98roland@gmail.com>
2022-12-29 21:23:15 +0100

    feat: added the ProductList component and it's scss file

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,8 +1,95 @@
 import "./ProductList.scss";
-type Props = {};
+import { FC } from "react";
+import ItemCard from "../ItemCard/ItemCard";
 
-const ProductList = (props: Props) => {
-  return <div className="product-list"></div>;
+type Props = {
+  category: string;
+  maxPrice: number;
+  sort: string;
+};
+
+const ProductList: FC<Props> = ({ category, maxPrice, sort }) => {
+  const DUMMY_DATA = [
+    {
+      id: 1,
+      image_1: require("../../images/cloth_1.jpg"),
+      title: "Superset clothes",
+      price: 44.99,
+      oldPrice: 22.99,
+      isNew: true,
+      category: "Clothes set",
+    },
+    {
+      id: 2,
+      image_1: require("../../images/cloth_2.jpg"),
+      title: "Nike shoes for men",
+      price: 24.99,
+      oldPrice: 22.99,
+      isNew: false,
+      category: "Men Shoes",
+    },
+    {
+      id: 3,
+      image_1: require("../../images/cloth_1.jpg"),
+      title: "Superset clothes",
+      price: 14.99,
+      oldPrice: 22.99,
+      isNew: true,
+      category: "Clothes set",
+    },
+    {
+      id: 4,
+      image_1: require("../../images/cloth_2.jpg"),
+      title: "Better Nike Shoes",
+      price: 104.99,
+      oldPrice: 140.99,
+      isNew: false,
+      category: "Men Shoes",
+    },
+    {
+      id: 5,
+      image_1: require("../../images/cloth_1.jpg"),
+      title: "Superset clothes",
+      price: 14.99,
+      oldPrice: 22.99,
+      isNew: true,
+      category: "Clothes set",
+    },
+    {
+      id: 6,
+      image_1: require("../../images/cloth_2.jpg"),
+      title: "Better Nike Shoes",
+      price: 104.99,
+      oldPrice: 140.99,
+      isNew: false,
+      category: "Men Shoes",
+    },
+    {
+      id: 7,
+      image_1: require("../../images/cloth_1.jpg"),
+      title: "Superset clothes",
+      price: 14.99,
+      oldPrice: 22.99,
+      isNew: true,
+      category: "Clothes set",
+    },
+    {
+      id: 8,
+      image_1: require("../../images/cloth_2.jpg"),
+      title: "Better Nike Shoes",
+      price: 104.99,
+      oldPrice: 140.99,
+      isNew: false,
+      category: "Men Shoes",
+    },
+  ];
+  return (
+    <div className="product-list">
+      {DUMMY_DATA.map((item) => (
+        <ItemCard key={item.id} item={item} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
