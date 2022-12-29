31dd07ead9b6cc4653298b6d8621317655a0d7aa
kerekesroland <kerekes98roland@gmail.com>
2022-12-29 17:03:52 +0100

    feat: added the FeaturedProducts and Categories components to the HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import "./Home.scss";
 import Slider from "../../components/Slider/Slider";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import Categories from "../../components/Categories/Categories";
 
 type Props = {};
 
@@ -7,6 +9,8 @@ const Home = (props: Props) => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts />
+      <Categories />
     </div>
   );
 };
