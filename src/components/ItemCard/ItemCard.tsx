423fdefa15fb58fac0e2f3286dd2930b371d4399
kerekesroland <kerekes98roland@gmail.com>
2022-12-29 17:02:08 +0100

    feat: ItemCard component and its responsive design

---
+++ src/components/ItemCard/ItemCard.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Item } from "../../models/Item";
+import "./ItemCard.scss";
+
+type Props = {
+  item: Item;
+};
+
+const ItemCard: FC<Props> = ({ item }) => {
+  return (
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
+        <div className="img-container">
+          <img src={item.image_1} alt="item" />
+        </div>
+        <div className="details">
+          <span className="name">{item.title}</span>
+          <span className="price">${item.price}</span>
+        </div>
+        <span className="category">{item.category}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default ItemCard;
