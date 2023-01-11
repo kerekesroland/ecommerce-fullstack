590465bdbc62ef6e2929fb53f293ce7e0de7d2cf
kerekesroland <kerekes98roland@gmail.com>
2023-01-11 18:39:48 +0100

    feat: added Fallback for the i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./i18";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
