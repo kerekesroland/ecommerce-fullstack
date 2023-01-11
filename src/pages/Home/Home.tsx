b964d1a9fd0c8c228955cd1cfed3cee02dcb5e6a
kerekesroland <kerekes98roland@gmail.com>
2023-01-11 18:41:33 +0100

    chore: used the ProfileTab on the homePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import "./Home.scss";
 import Slider from "../../components/Slider/Slider";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Categories from "../../components/Categories/Categories";
+import ProfileTab from "../../components/ProfileTab/ProfileTab";
 
 type Props = {};
 
@@ -9,6 +10,7 @@ const Home = (props: Props) => {
   return (
     <div className="home">
       <Slider />
+      <ProfileTab />
       <FeaturedProducts />
       <Categories />
     </div>
