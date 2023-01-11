7acff102d129c60f48d6d3bf8d50d381b48c8b9a
kerekesroland <kerekes98roland@gmail.com>
2023-01-11 18:39:38 +0100

    feat: integrated the i18n translation feature

---
+++ src/i18.ts
@@ -0,0 +1,42 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    fallbackLng: "en",
+    resources: {
+      en: {
+        translation: require("../../client/src/assets/translations/en.json"),
+      },
+      fr: {
+        translation: require("../../client/src/assets/translations/fr.json"),
+      },
+      de: {
+        translation: require("../../client/src/assets/translations/de.json"),
+      },
+    },
+    detection: {
+      order: ["querystring", "cookie"],
+      caches: ["cookie"],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+let currentLanguage = localStorage.getItem("i18nextLng");
+
+if (currentLanguage) {
+  i18n.changeLanguage(currentLanguage);
+} else {
+  currentLanguage = i18n.language;
+  localStorage.setItem("i18nextLng", currentLanguage);
+}
+
+export default i18n;
