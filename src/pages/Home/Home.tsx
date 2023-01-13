51ea7bd37cc05726602142f7481ac0b69fc436fa
kerekesroland <kerekes98roland@gmail.com>
2023-01-14 00:26:40 +0100

    remove: removed profiletab from the home page as it was too crowded

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,6 @@ import "./Home.scss";
 import Slider from "../../components/Slider/Slider";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Categories from "../../components/Categories/Categories";
-import ProfileTab from "../../components/ProfileTab/ProfileTab";
 
 type Props = {};
 
@@ -10,7 +9,6 @@ const Home = (props: Props) => {
   return (
     <div className="home">
       <Slider />
-      <ProfileTab />
       <FeaturedProducts />
       <Categories />
     </div>
