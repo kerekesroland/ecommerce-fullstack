be736c8c4799459bac81e5aeb3d43acabb90903a
kerekesroland <kerekes98roland@gmail.com>
2023-01-21 18:22:07 +0100

    feat: added the auth service file and functionalities

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,13 +1,34 @@
 import { ILoginUser, IRegisterUser } from "./models";
 import axios from "axios";
-export const authUrl = "http://localhost:1337/auth/local";
 
-export const registerUser = async (user: IRegisterUser) => {
+export const authUrl = "http://localhost:1337/api/auth/local";
+
+const registerUser = async (user: IRegisterUser) => {
   const res = await axios.post(`${authUrl}/register`, user);
+
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data));
+  }
+
   return res.data;
 };
 
-export const loginUser = async (user: ILoginUser) => {
+const loginUser = async (user: ILoginUser) => {
   const res = await axios.post(authUrl, user);
+
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data));
+  }
+
   return res.data;
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+export const authService = {
+  registerUser,
+  loginUser,
+  logout,
 };
