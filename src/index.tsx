b0e83207d201dcb5130a8670918544b2c9d68763
kerekesroland <kerekes98roland@gmail.com>
2023-01-21 18:21:41 +0100

    feat: added the redux store for the global state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./i18";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </Provider>
   </React.StrictMode>
 );
