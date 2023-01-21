f38c9ea28f1f5e013e22cd2f4a3a747d41450829
kerekesroland <kerekes98roland@gmail.com>
2023-01-21 18:25:05 +0100

    feat: added the redux store

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./slices/authSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+  },
+});
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
