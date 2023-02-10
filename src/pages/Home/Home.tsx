496238e0909d40d89808ae5f71b01108338afdbe
kerekesroland <kerekes98roland@gmail.com>
2023-02-10 20:33:50 +0100

    chore: updated the scss for the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,7 @@ import Slider from "../../components/Slider/Slider";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Categories from "../../components/Categories/Categories";
 
-type Props = {};
-
-const Home = (props: Props) => {
+const Home = () => {
   return (
     <div className="home">
       <Slider />
