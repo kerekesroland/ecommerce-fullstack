409bf043384fbf6f1b7064a4e67d930b61c32226
kerekesroland <kerekes98roland@gmail.com>
2023-02-10 20:32:00 +0100

    feat(WIP): created the profiledetails comp

---
+++ src/components/ProfileDetails/ProfileDetails.tsx
@@ -0,0 +1,8 @@
+import "./ProfileDetails.scss";
+type Props = {};
+
+const ProfileDetails = (props: Props) => {
+  return <div className="profile-details">Profile Details</div>;
+};
+
+export default ProfileDetails;
