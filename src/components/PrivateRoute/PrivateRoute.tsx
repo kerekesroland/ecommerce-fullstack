92b2bfc8efca360d6e815b2516a6ddfc47ab1a09
kerekesroland <kerekes98roland@gmail.com>
2023-02-18 20:05:49 +0100

    update: updated the PrivateRoute with the currentUser check

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,8 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { auth } from "../../firebase/config";
 
 const PrivateRoute = ({ children }: any) => {
-  return auth.currentUser ? children : <Navigate to="/auth" />;
+  const currentUser = localStorage.getItem("user");
+
+  return currentUser ? children : <Navigate to="/auth" />;
 };
 
 export default PrivateRoute;
