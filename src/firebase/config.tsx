3b2207b015216c0d534ee7666f58344978f985ff
kerekesroland <kerekes98roland@gmail.com>
2023-02-18 20:08:36 +0100

    remove: removed the comments from the config.tsx

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -3,8 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
