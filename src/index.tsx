e958918f4c1b5e39b907cf0dc70245fe301aba40
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 16:52:22 +0100

    feat: Added PersistGate for the index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,22 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./i18";
 import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const persistor = persistStore(store);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Suspense fallback={<div>Loading...</div>}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Suspense>
     </Provider>
   </React.StrictMode>
