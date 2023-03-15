a265825a38a93ceead473e039a2c4373fcb0f4b9
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 22:27:18 +0100

    feat: added Checkbox custom component with material ui and react

---
+++ src/components/CheckboxController/CheckBox.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import "./CheckboxController.scss";
+
+interface IProps {
+  label: string;
+  value: number | string;
+  onChange: (value: number | string, isChecked: boolean) => void;
+}
+
+function CheckboxController({ label, value, onChange }: IProps) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
+    onChange(value, isChecked);
+  };
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          value={value}
+          onChange={handleChange}
+          sx={{
+            "&.Mui-checked": {
+              color: "cornflowerblue", // custom color when checked
+            },
+          }}
+        />
+      }
+      label={label}
+    />
+  );
+}
+
+export default CheckboxController;
