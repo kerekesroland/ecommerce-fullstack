1f237eb3f7f7e4e90761c9e292d5c5f3a7251869
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 16:57:11 +0100

    chore: added treshold to the s;ider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,10 +1,9 @@
 import "./Slider.scss";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { motion } from "framer-motion";
-
-type Props = {};
+import { useInView } from "react-intersection-observer";
 
 const slides: Array<string> = [
   require("/Users/roland/work/ecommerce/client/src/images/girl_2-min-02.jpg"),
@@ -12,31 +11,34 @@ const slides: Array<string> = [
   require("/Users/roland/work/ecommerce/client/src/images/man_1-min-02.jpg"),
 ];
 
-const Slider = (props: Props) => {
+const Slider = () => {
   const [slide, setSlide] = useState<number>(0);
-  const [isInView, setIsInView] = useState(false);
+  const { ref, inView } = useInView({
+    threshold: 0.4,
+  });
 
   const prevSlide = () => {
     setSlide(slide === 0 ? 2 : (prev) => prev - 1);
   };
 
-  const nextSlide = () => {
+  const nextSlide = useCallback(() => {
     setSlide(slide === 2 ? 0 : (prev) => prev + 1);
-  };
-
-  // Auto slider every 3 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      nextSlide();
-    }, 5000);
-    return () => {
-      clearInterval(interval);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [slide]);
 
+  // Auto slider every 5 seconds if the image is in view
+  useEffect(() => {
+    if (inView) {
+      const interval = setInterval(() => {
+        nextSlide();
+      }, 5000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [inView, nextSlide, slide]);
+
   return (
-    <div className="slider">
+    <div className="slider" ref={ref}>
       <motion.div
         key={"slider"}
         className="container"
