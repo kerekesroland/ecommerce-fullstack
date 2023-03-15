d508d6970ced6ec9a8e3652d353567b0db316e19
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 16:55:21 +0100

    feat: Added a loader and its scss file for the loading animation

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import "./Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="container">
+      <span>R</span>
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
