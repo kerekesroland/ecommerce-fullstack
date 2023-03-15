a32afdf32e008cb4e95f2dfb12e65149d74029eb
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 16:58:38 +0100

    feat: added the products getter in the home screen from redux storage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,14 +2,37 @@ import "./Home.scss";
 import Slider from "../../components/Slider/Slider";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Categories from "../../components/Categories/Categories";
+import { getProducts } from "../../store/slices/productsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { AppDispatch, RootState } from "../../store/store";
+import Loader from "../../components/Loader/Loader";
 
 const Home = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const isLoading = useSelector((state: RootState) => state.loading.isLoading);
+  const products = useSelector((state: RootState) => state.products.products);
+
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(getProducts());
+      console.log("Product get called");
+    }
+  }, [dispatch, products.length]);
+
   return (
-    <div className="home">
-      <Slider />
-      <FeaturedProducts />
-      <Categories />
-    </div>
+    <>
+      {isLoading && (
+        <div className="loader-container">
+          <Loader />
+        </div>
+      )}
+      <div className="home">
+        <Slider />
+        <FeaturedProducts />
+        <Categories />
+      </div>
+    </>
   );
 };
 
