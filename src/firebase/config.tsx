907e0f7e4b5be2c3518111a0ee77971b8e6936bc
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 16:57:26 +0100

    feat: added getStorage for the firebase config

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
@@ -17,4 +17,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage();
 export const googleProvider = new GoogleAuthProvider();
