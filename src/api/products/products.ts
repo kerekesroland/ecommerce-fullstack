01717f931e58c2309458513fcfd9e5b1a6bc479b
kerekesroland <kerekes98roland@gmail.com>
2023-03-15 16:53:22 +0100

    feat: added the getProducts function to the api/products folder

--- src/api/products/products.ts
+++ src/api/products/products.ts
@@ -1,1 +1,20 @@
-export const baseUrl = "http://localhost:1337/api/products";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/config";
+
+const productsRef = "products";
+
+const getProducts = async () => {
+  const productDocs = await getDocs(collection(db, productsRef));
+  const products: any = [];
+  productDocs.forEach((product) =>
+    products.push({
+      id: product.id,
+      ...product.data(),
+    })
+  );
+  return products;
+};
+
+export const productsService = {
+  getProducts,
+};
