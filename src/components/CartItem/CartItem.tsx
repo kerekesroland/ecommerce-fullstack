19644f73b3ee3ac605e9638ee1f59262c581ba07
kerekesroland <kerekes98roland@gmail.com>
2023-03-19 13:30:51 +0100

    chore: cartItem now uses dynamic data

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -2,18 +2,22 @@ import "./CartItem.scss";
 import { FC } from "react";
 import { ICartItem } from "../../models/ICartItem";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { removeFromCart } from "../../store/slices/cartSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
 
 type Props = {
   item: ICartItem;
 };
 
 const CartItem: FC<Props> = ({ item }) => {
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <div className="cart-item">
       <div className="left">
         <div className="img-container">
-          <img src={item.image_1} alt={item.title} />
-          <span>1</span>
+          <img src={item.image} alt={item.title} />
+          <span>{item?.quantity}</span>
         </div>
       </div>
 
@@ -21,7 +25,7 @@ const CartItem: FC<Props> = ({ item }) => {
         <h1>{item.title}</h1>
         <p>Lorem ipsum dolor sit amet...</p>
       </div>
-      <div className="end">
+      <div className="end" onClick={() => dispatch(removeFromCart(item.id))}>
         <span>${item.price}</span>
         <DeleteIcon style={{ fill: "#DC143C	" }} className="delete-icon" />
       </div>
