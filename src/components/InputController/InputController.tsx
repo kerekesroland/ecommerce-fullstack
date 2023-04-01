a59ff431fc79a27d848f8ecde4710ccd05af8091
kerekesroland <kerekes98roland@gmail.com>
2023-04-01 21:53:59 +0200

    Feat: created the InputController component and it's scss file

---
+++ src/components/InputController/InputController.tsx
@@ -0,0 +1,32 @@
+import styles from "./InputController.module.scss";
+
+interface IProps {
+  register: any;
+  placeholder: string;
+  defaultName: string;
+  name: string;
+}
+
+const InputController = ({
+  register,
+  placeholder,
+  defaultName,
+  name,
+}: IProps) => {
+  return (
+    <div className={styles.input_controller}>
+      <label>{name}</label>
+      <div className={styles.input_container}>
+        <input
+          // {...register(defaultName)}
+          type="text"
+          minLength={4}
+          placeholder={placeholder || defaultName}
+          required
+        />
+      </div>
+    </div>
+  );
+};
+
+export default InputController;
