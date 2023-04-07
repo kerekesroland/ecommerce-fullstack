e4e2686ab7e85f5be31e318872090b83c716a21e
kerekesroland <kerekes98roland@gmail.com>
2023-04-07 21:22:46 +0200

    Feat: added Plans to the pages file and it's scss file for the styling

--- src/pages/Plans/Plans.tsx
+++ src/pages/Plans/Plans.tsx
@@ -1,9 +1,87 @@
-import "./Plans.scss";
+import { useEffect, useState } from "react";
+import BronzeCard from "../../components/ProfilePlan/BronzeCard/BronzeCard";
+import GoldCard from "../../components/ProfilePlan/GoldCard/GoldCard";
+import SilverCard from "../../components/ProfilePlan/SilverCard/SilverCard";
+import {
+  createPremiumBronzeSession,
+  createPremiumSilverSession,
+  createPremiumGoldSession,
+} from "../../stripe/createCheckoutSession";
+import styles from "./Plans.module.scss";
+import { auth } from "../../firebase/config";
+import Loader from "../../components/Loader/Loader";
+import cancelSubscription from "../../stripe/cancelSubscription";
+import getSubscriptionId from "../../stripe/getSubscriptionId";
 
-type Props = {};
+const Plans = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [userSubscriptionId, setUserSubscriptionId] = useState<string>("");
 
-const Plans = (props: Props) => {
-  return <div>Plans</div>;
+  useEffect(() => {
+    const getUserSubscription = async () => {
+      if (!auth.currentUser) return;
+      const subscriptionId: string = await getSubscriptionId(
+        auth?.currentUser?.uid
+      );
+      setUserSubscriptionId(subscriptionId);
+    };
+    getUserSubscription();
+  }, []);
+
+  const handleBronzeSubscription = async (userId: string) => {
+    await createPremiumBronzeSession(userId);
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+    setLoading(false);
+  };
+  const handleSilverSubscription = async (userId: string) => {
+    await createPremiumSilverSession(userId);
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+    setLoading(false);
+  };
+  const handleGoldSubscription = async (userId: string) => {
+    await createPremiumGoldSession(userId);
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return (
+      <div className="signatureLoader">
+        <Loader />
+      </div>
+    );
+  }
+  return (
+    <div className={styles.plans}>
+      <BronzeCard
+        activateSubscription={handleBronzeSubscription}
+        cancelSubscription={cancelSubscription}
+        userSubscriptionId={userSubscriptionId}
+        activate={true}
+        userId={auth?.currentUser?.uid}
+      />
+      <SilverCard
+        activateSubscription={handleSilverSubscription}
+        cancelSubscription={cancelSubscription}
+        userSubscriptionId={userSubscriptionId}
+        activate={true}
+        userId={auth?.currentUser?.uid}
+      />
+      <GoldCard
+        activateSubscription={handleGoldSubscription}
+        cancelSubscription={cancelSubscription}
+        userSubscriptionId={userSubscriptionId}
+        activate={true}
+        userId={auth?.currentUser?.uid}
+      />
+    </div>
+  );
 };
 
 export default Plans;
