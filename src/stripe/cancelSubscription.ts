9c5f89cd851453713e49c1a0b6db0a1b49ac42e5
kerekesroland <kerekes98roland@gmail.com>
2023-04-07 21:23:17 +0200

    Feat: cancelSubscription.ts added for canceling subscriptions

---
+++ src/stripe/cancelSubscription.ts
@@ -0,0 +1,26 @@
+import { initializeStripe } from "./initializeStripe";
+
+export default async function cancelSubscription(subscription_key?: string) {
+  const stripe = await initializeStripe();
+  if (stripe) {
+    const apiKey = process.env.REACT_APP_stripe_secret_key;
+
+    const headers = {
+      Authorization: `Bearer ${apiKey}`,
+    };
+
+    fetch(`https://api.stripe.com/v1/subscriptions/${subscription_key}`, {
+      method: "DELETE",
+      headers: headers,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to cancel subscription.");
+        }
+        console.log(`Subscription ${subscription_key} is now canceled.`);
+      })
+      .catch((error) => {
+        console.error("Error canceling subscription:", error);
+      });
+  }
+}
