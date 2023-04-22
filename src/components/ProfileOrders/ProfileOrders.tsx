796fc110704024d98e951a606905a6055c7a6024
kerekesroland <kerekes98roland@gmail.com>
2023-04-22 20:42:00 +0200

    Feat: Added profile orders functionality finally

--- src/components/ProfileOrders/ProfileOrders.tsx
+++ src/components/ProfileOrders/ProfileOrders.tsx
@@ -1,40 +1,64 @@
+import { useEffect, useState } from "react";
 import Order from "../Order/Order";
 import "./ProfileOrders.scss";
+import { getOrders } from "../../stripe/getOrders";
+import { User } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+import { toggleLoading } from "../../store/slices/loadingSlice";
+import Loader from "../Loader/Loader";
 
 export interface IOrder {
   id: string;
-  price: number;
-  date: string;
+  status: string;
+  latest_charge: string;
+  amount: number;
+  description: string;
+  createdAt: string;
 }
 
-const orders: Array<IOrder> = [
-  {
-    id: "54we634",
-    price: 310,
-    date: "02.07.2023 - 09:19",
-  },
-  {
-    id: "e312332",
-    price: 185,
-    date: "01.05.2022 - 14:19",
-  },
-  {
-    id: "94e7h23",
-    price: 550,
-    date: "01.22.2023 - 16:14",
-  },
-];
+const ProfileOrders = ({ user }: { user: User | null }) => {
+  const [orders, setOrders] = useState<IOrder[]>([]);
+  const dispatch: AppDispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-const ProfileOrders = () => {
+  useEffect(() => {
+    const orders = async () => {
+      setIsLoading(true);
+      if (user) {
+        const res = await getOrders(user.uid);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+
+        setOrders(
+          res.data.filter(
+            (order: IOrder) =>
+              order.description !== "Subscription creation" &&
+              order.status === "succeeded"
+          )
+        );
+      }
+    };
+    orders();
+  }, [user, dispatch]);
+
   return (
-    <div className="orders">
-      {orders.map((order) => (
-        <div className="profile-orders" key={order.id}>
-          <Order order={order} />
-          <hr className="profile-order-separator" />
+    <>
+      {isLoading && (
+        <div className="loader-container">
+          <Loader />
         </div>
-      ))}
-    </div>
+      )}
+      <div className="orders">
+        {orders.map((order) => (
+          <div className="profile-orders" key={order.id}>
+            <Order order={order} />
+            <hr className="profile-order-separator" />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
